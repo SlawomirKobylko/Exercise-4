8f0508b8fe9a80cf0f258af18252b5e59f8b46af
Sławek <Sawek@SLAWEK-PC>
2022-06-29 14:46:18 +0200

    Dodaj pliki projektów.

---
+++ Exercise 4/SzczegolyGrupy.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace Exercise_4
+{
+    /// <summary>
+    /// Logika interakcji dla klasy SzczegolyGrupy.xaml
+    /// </summary>
+    public partial class SzczegolyGrupy : Window
+    {
+        public SzczegolyGrupy(XmlNode grupa)
+        {
+            DataContext = grupa;
+            InitializeComponent();
+        }
+    }
+}
